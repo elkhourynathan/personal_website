c0787d2b7a84e09ca47006ac5d6c60fe2c0a80b0
elkhourynathan <elkhourynathan@gmail.com>
2025-06-21 14:08:58 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Uncomment and set these if deploying to a subpath:
+  // basePath: '/personal_website',
+  // assetPrefix: '/personal_website/',
 };
 
 export default nextConfig;
